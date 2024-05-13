37e285f2a4a54211e987f561292d0ac41a45db95
Euris1935 <ep2022-1935@uce.edu.do>
2024-05-13 10:27:13 -0400

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla_de_multiplicar_de_un_numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******** Tabla de Multiplicacion de Numeros ********");
+
+            Console.WriteLine("Ingrese un numero");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tabla de multiplicar del : "+ numero);
+            
+
+            for (int i = 0; i < 13; i++)
+            {
+                  int resultado = numero * i;
+                Console.WriteLine(numero + " x " + i + " = " + resultado);
+
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
