d97e65d7e052d94d60b3886b9317d329bcdfa773
Euris1935 <ep2022-1935@uce.edu.do>
2024-05-13 10:30:31 -0400

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion_Cardinal_To_Ordinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Ingrese el numero Cardinal : ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                string ordinal;
+
+                if (i % 100 == 11 || i % 100 == 12 || i % 100 == 13)
+                {
+                    ordinal = i + "th";
+                }
+                else
+                {
+                    switch (i % 10)
+                    {
+                        case 1: ordinal = i + "st"; break;
+                        case 2: ordinal = i + "nd"; break;
+                        case 3: ordinal = i + "rd"; break;
+                        default: ordinal = i + "th"; break;
+                    }
+                }
+
+                Console.Write(ordinal + " ");
+            }
+            Console.ReadLine();
+        }
+
+    }
+    
+} 
+
+
+
+
+
